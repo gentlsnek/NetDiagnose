8db147fd2df167374c27ac686d371de2187c2fac
gentlesnek <erwinpimenta1644@gmail.com>
2024-11-06 18:19:31 +0530

    finished interface info

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ sys.path.append('windows')
 if sys.platform == 'linux':
     from linux.network_con import ping_test, dns_lookup, trace_route
     from linux.speedtest import speed_test
+    from linux.network_interface import network_interfaces_info
 #windows modules
 if sys.platform == 'win32':
     from windows.network_con import ping_test, dns_lookup, trace_route
     from windows.speedtest import speed_test
+    from windows.network_interface import network_interfaces_info
 
 
 
 
 def main():
-        print("Network Diagnostic Tool")
-        option = input("choose option \n1.Network Information    2.Speed Test\n3.Network Interface      4.Wifi Analysis\n5.Error Reporting        6.Security Inforamtion \n7.Exit\n")
+    print("Network Diagnostic Tool")
+    
+    while True:
+        
+        print("choose option \n1.Network Information    2.Speed Test\n3.Network Interface      4.Wifi Analysis\n5.Error Reporting        6.Security Inforamtion \n7.Exit\n")
+        option = input("Enter Option: ")
 
         if option == '1':
              ping_test("8.8.8.8")
@@ -27,7 +33,7 @@ def main():
         elif option == '2':
              speed_test()
         elif option == '3':
-             pass
+             network_interfaces_info()
         elif option == '4':
              pass
         elif option == '5':
@@ -35,7 +41,8 @@ def main():
         elif option == '6':
              pass
         elif option == '7':
-             exit()
+             print("Exiting")
+             break
         else:
             print("Invalid Option")
                                  
